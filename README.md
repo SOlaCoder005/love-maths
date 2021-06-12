c4d75dc905a65e3378c0258a7187169b105602fe
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-12 09:55:05 +0100

    script.js: added doc sting to the runGame function

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 
 
  - **Single Responsibility Principle:** Suggested that each funtion should have one key role only. See .js file for more info/example.
+
+ **Doc Strings:**
+ - Used in JS file to tell coders what a specific function does. 
+ - Syntax:  [/** */]
+ -
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 /* 
     EVENT HANDLERS BELOW!
-    Two Types: 
-    1) Code is executed when page has finished loading the content
-    2) Code is executed once a button is clicked
-    Key Tutorial Source: [https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+2021_T1/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/?child=first]
+    * Two Types: 
+        1) Code is executed when page has finished loading the content
+        2) Code is executed once a button is clicked
+    Tutorial Source: [https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+2021_T1/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/?child=first]
 */
 
 // NOTE: Once page has finished loading the code in this block will execute
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
     /* 
-        for (let i = 0; i < buttons.length; i ++) {} 
-        NOTE: [i = 0 - will set i to zero] [i < button.length - checks to see if i is less than the button array] [i++ - this will increment i by 1]
-        The for loop above is an older syntax, as such a newer syntax to achieve this effect has been used below: 
+        SYNTAX: [ for (let i = 0; i < buttons.length; i ++) {} ] 
+        * NOTE: [i = 0 - will set i to zero] [i < button.length - checks to see if i is less than the button array] [i++ - this will increment i by 1]
+        * The for loop above is an older syntax, as such a newer syntax to achieve this effect has been used below: 
     */
         for (let button of buttons) {
             button.addEventListener("click", function() {
@@ -28,22 +28,34 @@ document.addEventListener("DOMContentLoaded", function() {
          }
 
         /* 
-            Benifits of synatx above: 
-            1) Goes through buttons array and return each element in array. 
-            2) This data shall be stored in the specified variable on each iteration attempt
-            3) Reads cleaner for users
+            * Benifits of synatx above: 
+                1) Goes through buttons array and return each element in array. 
+                2) This data shall be stored in the specified variable on each iteration attempt
+                3) Reads cleaner for users
         */
 
         // NOTE: Inside this code block [this.] referes to a specific button 
 })
 
 /*
-    Using the SRP for each function below: 
-    Avoid creating too many variables in the 'global scope'. To do this, make sure everything is contained within a fucntion block of code.
+    * Using the SRP for each function below: 
+    * Avoid creating too many variables in the 'global scope'. To do this, make sure everything is contained within a fucntion block of code.
     Source: [https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+2021_T1/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/?child=first]
 */
 
+/**
+This [Math.floor()......] creates two random number between 1 and 25. 
+The function supports main game loop. It is: 
+    1) Called whent the script is first loaded and 
+    2) after users' answers has been processed. 
+*/
 function runGame() {
+/* 
+    Source Tutorial: [https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+2021_T1/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/?child=first]
+    * The console tab in GC Dev Tools was used to test beforehand, before adding this to the .js file
+*/
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
 
 }
 
